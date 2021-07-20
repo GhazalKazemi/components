471e5c41e7edb660a08cf5a2adbd19260dc166a2
Ghazal <ghazal.kazemi@gmail.com>
2021-07-20 15:43:41 +1000

    Import shared component for divider widget

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { CollectionsRoutingModule } from './collections-routing.module';
-import { CollectionsHomeComponent } from './collections-home/collections-home.component';
-import { TableComponent } from './table/table.component';
-
+import {CollectionsRoutingModule} from './collections-routing.module';
+import {CollectionsHomeComponent} from './collections-home/collections-home.component';
+import {TableComponent} from './table/table.component';
+import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
-    declarations: [
-        CollectionsHomeComponent,
-        TableComponent
-    ],
-    exports: [],
-    imports: [
-        CommonModule,
-        CollectionsRoutingModule
-    ]
+  declarations: [
+    CollectionsHomeComponent,
+    TableComponent
+  ],
+  exports: [],
+  imports: [
+    CommonModule,
+    CollectionsRoutingModule,
+    SharedModule
+  ]
 })
-export class CollectionsModule { }
+export class CollectionsModule {
+}
