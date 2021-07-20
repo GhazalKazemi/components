b33439379f7a56a545d949806997a2cf9a15fa47
Ghazal <ghazal.kazemi@gmail.com>
2021-07-20 16:13:06 +1000

    Add extra column to the reusable table

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -8,12 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class CollectionsHomeComponent implements OnInit {
 
   data = [
-    {name: "John", age: 25, job: "Engineer"},
-    {name: "Elyse", age: 23, job: "Designer"},
-    {name: "Judy", age: 24, job: "Engineer"}
+    {name: "John", age: 25, job: "Engineer", employed: true},
+    {name: "Elyse", age: 23, job: "Designer", employed: false},
+    {name: "Judy", age: 24, job: "Engineer", employed: true}
   ];
 
   headers = [
+    {key:'employed', label: 'Has a Job'},
     {key: 'name', label: "Name"},
     {key: 'age', label: 'Age'},
     {key: 'job', label: "Job"}
