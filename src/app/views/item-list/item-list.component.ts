a003b1e32956ea93f4e02194a5a7b12351b9b866
Ghazal <ghazal.kazemi@gmail.com>
2021-07-21 13:45:15 +1000

    Use input() to refer to array of images

--- src/app/views/item-list/item-list.component.ts
+++ src/app/views/item-list/item-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item-list',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemListComponent implements OnInit {
 
+  @Input() items: any = [];
   constructor() { }
 
   ngOnInit(): void {
