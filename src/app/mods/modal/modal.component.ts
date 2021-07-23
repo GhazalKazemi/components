f99f611c5fd70d0788a7b1535341688bc763ce37
Ghazal <ghazal.kazemi@gmail.com>
2021-07-23 15:33:31 +1000

    Append app-modal to body to make the modal position relevent to app body

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(private element: ElementRef) { }
 
   ngOnInit(): void {
+    document.body.append(this.element.nativeElement);
   }
 
 }
